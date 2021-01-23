aa627597cbc1b67046ec21c008c16ccb1be0d8fb
Blockman <71958682+Blockman18@users.noreply.github.com>
2021-01-22 18:33:49 -0600

    Update does nothing just need github post.go

--- does nothing just need github post.go
+++ does nothing just need github post.go
@@ -4,18 +4,15 @@ package main
 
 import (
 	"fmt"
-	"time"
+	
 	  
-	"github.com/pkg/browser"
+	
 
 
 )
 
 func main() {
 	fmt.Print("so u opened my exe okay!")
-	OpenURL("https://github.com/Blockman18")
-	time.Sleep(5 * time.Second)
-	fmt.Print("thanks now follow the github!")
 	time.Sleep(3 * time.Second)
 
 
