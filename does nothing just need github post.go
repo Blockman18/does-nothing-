3f470df4eb32f9f5a4c4773981e8d876be2f81ac
Blockman <71958682+Blockman18@users.noreply.github.com>
2021-01-22 18:32:59 -0600

    Add files via upload

---
+++ does nothing just need github post.go
@@ -0,0 +1,34 @@
+package main 
+
+
+
+import (
+	"fmt"
+	"time"
+	  
+	"github.com/pkg/browser"
+
+
+)
+
+func main() {
+	fmt.Print("so u opened my exe okay!")
+	OpenURL("https://github.com/Blockman18")
+	time.Sleep(5 * time.Second)
+	fmt.Print("thanks now follow the github!")
+	time.Sleep(3 * time.Second)
+
+
+
+
+	
+
+
+
+
+
+
+
+
+}
+	
